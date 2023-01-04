412b5204feefaa201eba8316a4885fde47803b00
binjip978 <pdp.eleven11@gmail.com>
2023-01-04 14:43:04 +0200

    vmprint

--- kernel/vm.c
+++ kernel/vm.c
@@ -437,3 +437,27 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void vmprintf_rec(pagetable_t pagetable, int level, char *pre) {
+    for (int i = 0; i < 512; i++) {
+      pte_t pte = pagetable[i];
+      if ((pte & PTE_V) && (pte & (PTE_R|PTE_W|PTE_X)) == 0) {
+        pagetable_t child = (pagetable_t) PTE2PA(pte);
+        printf("%s %d: pte %p pa %p\n", pre, i, pte, child);
+        if (level == 1) {
+            vmprintf_rec(child, level + 1, "....");
+        } else {
+            vmprintf_rec(child, level + 1, "......");
+        }
+      } else if (pte & PTE_V) {
+        printf("%s %d: pte %p pa %p\n", pre, i, pte, PTE2PA(pte));
+        return;
+      }
+    }
+}
+
+void vmprint(pagetable_t pagetable) {
+    printf("page table %p\n", pagetable);
+    vmprintf_rec(pagetable, 1, "..");
+}
+
